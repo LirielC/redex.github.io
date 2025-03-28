e16679b78f7c3af8a3546bec93bc38f0a293cd14
LirielC <lirielcastroreis@gmail.com>
2025-03-27 21:45:01 -0300

    Corrigindo configurações de roteamento para GitHub Pages

--- arquitetura-rede-consultoria/next.config.mjs
+++ arquitetura-rede-consultoria/next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/-NetArchitect-.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/-NetArchitect-.github.io' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
