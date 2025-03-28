936c40539755af0ffaf1136afd2ad0e55a465642
LirielC <lirielcastroreis@gmail.com>
2025-03-27 21:31:20 -0300

    Simplificando configurações para resolver problemas de build

--- arquitetura-rede-consultoria/next.config.mjs
+++ arquitetura-rede-consultoria/next.config.mjs
@@ -1,40 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
-    domains: [
-      "source.unsplash.com",
-      "images.unsplash.com",
-      "ext.same-assets.com",
-      "ugc.same-assets.com",
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ext.same-assets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ugc.same-assets.com",
-        pathname: "/**",
-      },
-    ],
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/-NetArchitect-.github.io' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/-NetArchitect-.github.io' : '',
+  basePath: '/-NetArchitect-.github.io',
 };
 
 export default nextConfig;
